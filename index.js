c02026230121ba6c737ec29dab4ff1e4a55a3d41
Muharim <muharim508@gmal.com>
2023-05-15 22:40:34 +0700

    add error empty

--- index.js
+++ index.js
@@ -1,8 +1,66 @@
 function Submit() {
-    var d1 = document.getElementById('hari').value;
-    var m1 = document.getElementById('bulan').value;
-    var y1 = document.getElementById('tahun').value;
-  
+    var d1 = document.getElementById('iDay').value;
+    var m1 = document.getElementById('iMonth').value;
+    var y1 = document.getElementById('iYear').value;
+    var errH = document.getElementById('error-hari').classList;
+    var errB = document.getElementById('error-bulan').classList;
+    var errT = document.getElementById('error-tahun').classList;
+    var td = document.getElementById('tDay').classList;
+    var tb = document.getElementById('tMonth').classList;
+    var tt = document.getElementById('tYear').classList;
+    var ih = document.getElementById('iDay').classList;
+    var ib = document.getElementById('iMonth').classList;
+    var it = document.getElementById('iYear').classList;
+
+    if (d1 === "" || m1 === "" || y1 === "") {
+      if (d1 === ""){
+        errH.remove('hidden')
+        errH.add('block')
+        td.remove('text-smokeGray')
+        td.add('text-red-400')
+        ih.remove('border-offWhite')
+        ih.add('border-red-400')
+      }
+      
+      if (m1 === ""){
+        errB.remove('hidden')
+        errB.add('block')
+        tb.remove('text-smokeGray')
+        tb.add('text-red-400')
+        ib.remove('border-offWhite')
+        ib.add('border-red-400')
+      }
+      if (y1 === ""){
+        errT.remove('hidden')
+        errT.add('block')
+        tt.remove('text-smokeGray')
+        tt.add('text-red-400')
+        it.remove('border-offWhite')
+        it.add('border-red-400')
+      }
+      return;
+    }
+    errH.add('hidden')
+    errH.remove('block')
+    td.add('text-smokeGray')
+    td.remove('text-red-400')
+    ih.add('border-offWhite')
+    ih.remove('border-red-400')
+
+    errB.add('hidden')
+    errB.remove('block')
+    tb.add('text-smokeGray')
+    tb.remove('text-red-400')
+    ib.add('border-offWhite')
+    ib.remove('border-red-400')
+
+    errT.add('hidden')
+    errT.remove('block')
+    tt.add('text-smokeGray')
+    tt.remove('text-red-400')
+    it.add('border-offWhite')
+    it.remove('border-red-400')
+
     var date = new Date();
     var d2 = date.getDate();
     var m2 = 1 + date.getMonth();
@@ -21,5 +79,12 @@ function Submit() {
     var m = m2 - m1;
     var y = y2 - y1;
   
-    document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
+    document.getElementById('years').innerText= y;
+    document.getElementById('months').innerText= m;
+    document.getElementById('days').innerText= d;
   }
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  Submit();
+})
+
